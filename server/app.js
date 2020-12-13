2647f8fb92eaf770cbcc0dba76f6952d5f5e0afb
Ohad <ohad@salaryo.com>
2020-12-13 21:31:26 +0200

    Shows todos in the UI

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const cors = require('cors')
+
 const { PORT } = require("./settings");
 
 const app = express();
 
+
+app.use(cors())
 app.use(express.json())
 
 app.use(function(req, res, next) {
